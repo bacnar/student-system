e14a685d2f0f7cf831b736d7305829baa48eca88
Rok Bacnar <rok.bacnar@gmail.com>
2020-06-22 10:23:38 +0200

    Added auth service and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
